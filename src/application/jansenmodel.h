e79c419c8268a112906a04729ad471483ec720d6
takumi@TMBP <takumi.ryochin@gmail.com>
2015-09-09 08:49:34 +0900

    [add]前回のコミットで追加忘れのファイルの追加
    Chev-Jansen-K3の座標に関する関数などが実装されたファイルを追加します．

---
+++ src/application/jansenmodel.h
@@ -0,0 +1,56 @@
+#ifndef JANSENMODEL_H
+#define JANSENMODEL_H
+#include <stdint.h>
+#include "coordinate.h"
+
+// マシンの中心から車輪までの距離([cnt]，1700/2pi)
+#define MACHINE_RADIUS_CNT	270.5634033
+// マシンの中心から車輪までの距離([mm]，0.22*1700/2pi)
+#define MACHINE_RADIUS_MM	59.52394872
+
+// 1cntで進む距離[mm/cnt]．
+#define MM_PER_CNT		0.22
+
+// 記録できる仮想原点の数
+#define COOD_LIST_SIZE	16
+
+/**
+ * 現在いる位置を原点とする．使い道あるかな？？
+ */
+void set_origin(void);
+/**
+ * 引数で指定された右左のカウント数だけ，座標を進めます．
+ * @param cnt_r 単位時間あたりの右車輪のカウント数
+ * @param cnt_l 単位時間あたりの左車輪のカウント数
+ */
+void move_one_step(int16_t cnt_r, int16_t cnt_l);
+/**
+ * 現在の位置を，相対座標取得関数で使用する，仮原点として記録します．
+ * 引数で指定されたインデックスにより，この仮原点を参照します．
+ * @param cood_index 仮原点のインデックス．
+ */
+void set_rel_origin(uint8_t cood_index);
+/**
+ * {@code set_origin()}で指定した点を原点とした時の現在の座標を極座標で返します．
+ * @return 現在の絶対座標．
+ */
+PoleCood get_abs_polecood();
+/**
+ * {@code set_origin()}で指定した点を原点とした時の現在の座標を直交座標で返します．
+ * @return 現在の絶対座標．
+ */
+RectCood get_abs_rectcood();
+/**
+ * {@code set_rel_origin(uint8_t cood_index)}で指定した座標からの相対座標を極座標で返します．
+ * @param cood_index {@code set_rel_origin(uint8_t cood_index)}で指定した，仮原点のインデックス．
+ * @return 指定された座標からの相対座標．
+ */
+PoleCood get_rel_polecood(uint8_t cood_index);
+/**
+ * {@code set_rel_origin(uint8_t cood_index)}で指定した座標からの相対座標を極座標で返します．
+ * @param cood_index {@code set_rel_origin(uint8_t cood_index)}で指定した，仮原点のインデックス．
+ * @return 指定された座標からの相対座標．
+ */
+RectCood get_rel_rectcood(uint8_t cood_index);
+
+#endif /** JANSENMODEL_H */
