ca2c2df7e75a708b7ad1b859626e0e3e4bab89bd
takumi@TMBP <takumi.ryochin@gmail.com>
2015-09-13 02:30:30 +0900

    [modify]シリアル系便利関数の実装
    便利な関数を実装しました．

--- src/application/serial.c
+++ src/application/serial.c
@@ -1,5 +1,8 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #define FOSC 20000000
 #define BAUD 57600
@@ -26,3 +29,77 @@ void init_serial() {
 }
 
 // printf is declared at stdio
+
+void printstr(char *str) {
+	while (*str != '\0') {
+		uart_putchar(*str);
+		str++;
+	}
+}
+void printlnstr(char *str) {
+	printstr(str);
+	uart_putchar('\n');
+	uart_putchar('\r');
+}
+void printint(int16_t num, uint16_t radix) {
+	// 文字列格納する変数(2byteは最大5桁、符号一桁、終端文字1文字)
+	char str[7];
+	// ポインタ
+	char *ptr;
+
+	// 一応初期化
+	for (int i=0; i<7; i++) {
+		str[i] = '\0';
+	}
+	// ポインタも
+	ptr = str;
+
+	if (num < 0) {
+		// 符号
+		*ptr = '-';
+		// ポインタ進める
+		ptr ++;
+		// 正規化
+		num = -num;
+	}
+
+	for (int i=4; i>=0; i--) {
+		// 最大5桁
+
+		// その桁の文字取得(右詰め)
+		*(ptr+i) = '0' + (num%radix);
+		// 桁をずらす
+		num /= radix;
+
+		if (num == 0) {
+			// もう終わり
+
+			// 右詰めしたものをずらして行く
+			for (int j=0; j<=4-i; j++) {
+				*(ptr+j) = *(ptr+j+i);
+			}
+
+			// 最後はゼロ埋め．
+			*(ptr+5-i) = '\0';
+
+			// 出る
+			break;
+		}
+	}
+
+	// 作った文字をプリント
+	printstr(str);
+}
+void printlnint(int16_t num, uint16_t radix) {
+	printint(num, radix);
+	uart_putchar('\n');
+	uart_putchar('\r');
+}
+char getch() {
+	// return dequeue();
+	while( !(UCSR0A & 0b10000000) );
+	return UDR0;
+}
+void putch(char c) {
+	uart_putchar(c);
+}
--- src/application/serial.h
+++ src/application/serial.h
@@ -6,3 +6,39 @@
 void init_serial();
 
 int printf(const char *fmt, ...);
+/**
+ * 引数で指定された文字列を、'\0'が来るまで連続で出力し続けます．
+ * @param str 文字列ポインタ
+ */
+void printstr(char *str);
+/**
+ * 引数で指定された数値を、引数で指定された基数に従って文字列に変換し、表示します．
+ * @param num 表示する文字列
+ * @param radix 基数
+ */
+void printint(int16_t num, uint16_t radix);
+/**
+ * 引数で指定された文字列を、'\0'が来るまで連続で出力し続けます．
+ * 最後に改行を出力します．
+ * @param str 文字列ポインタ
+ */
+void printlnstr(char *str);
+/**
+ * 引数で指定された数値を、引数で指定された基数に従って文字列に変換し、表示します．
+ * 最後に改行を出力します．
+ * @param num 表示する文字列
+ * @param radix 基数
+ */
+void printlnint(int16_t num, uint16_t radix);
+/**
+ * バッファから一文字読み出します．
+ * この時、エコーバックはしません．
+ * また、バッファが空の時は-1を返します．
+ * @return バッファの先頭の文字．バッファが空の時は-1．
+ */
+char getch();
+/**
+ * 引数で指定された文字を出力します
+ * @param c 出力する文字．
+ */
+void putch(char c);
