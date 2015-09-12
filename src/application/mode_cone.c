b2fed3e9083b5d8d5dcb84e53751968ca48a072b
kasai <fishmeat37820+git@gmail.com>
2015-09-13 06:23:56 +0900

    [add] アーム上げ下げの動作を実装．動作確認済

--- src/application/mode_cone.c
+++ src/application/mode_cone.c
@@ -1,4 +1,4 @@
-##include "mode_cone.h"
+#include "mode_cone.h"
 #include "arm.h"
 #include <util/delay.h>
 
@@ -22,13 +22,48 @@ void mode_cone_loop(void){
 
 // 調整用
 void adj(void){
-	int arm_l = (ADC_Solo(0)<<3);
-	int arm_u = (ADC_Solo(1)<<3);
 
-	move_arms(arm_l, arm_u);
+	int arm_l = get_lowerArm_position();// (ADC_Solo(0)<<3);
+	int arm_u = get_upperArm_position();// (ADC_Solo(1)<<3);
 
-	printf("%d,\t%d\n\r",arm_l,arm_u);
+	//move_arms(arm_l, arm_u);
 
-	_delay_ms(100);
+
+	set_arm_insert();
+	_delay_ms(1000);
+	printf("%d,\t%d\n\r",get_lowerArm_position(),get_upperArm_position());
+
+	set_arm_grab();
+	_delay_ms(1000);
+	printf("%d,\t%d\n\r",get_lowerArm_position(),get_upperArm_position());
+
+	set_arm_grab();
+	_delay_ms(1000);
+
+	set_arm_holdUp();
+	_delay_ms(1000);
+
+	set_arm_release();
+	_delay_ms(1000);
 }
 
+
+void set_arm_insert(void){
+	move_arms(ARM_L_POSITION_INSERT, ARM_U_POSITION_INSERT);
+}
+
+void set_arm_grab(void){
+	move_arms(ARM_L_POSITION_INSERT, ARM_U_POSITION_HOLD);
+}
+
+void set_arm_holdUp(void){
+	// ゆっくり上げる
+	move_arm_lower_slow(ARM_L_POSITION_HOLD);
+}
+
+void set_arm_release(void){
+	// 上側アームはリリース位置にセットしておく
+	move_arms(ARM_L_POSITION_HOLD, ARM_U_POSITION_RELEASE);
+	// ゆっくり下ろす
+	move_arm_lower_slow(ARM_L_POSITION_RELEASE);
+}
--- src/application/mode_cone.h
+++ src/application/mode_cone.h
@@ -1,6 +1,40 @@
 #ifndef MODE_CONE_H
 #define MODE_CONE_H
 
+// アーム位置
+#define ARM_L_POSITION_INSERT  0	// コーンをセットできる位置
+#define ARM_U_POSITION_INSERT  320	// コーンをセットできる位置
+#define ARM_L_POSITION_HOLD    1000	// 持ち歩き位置
+#define ARM_U_POSITION_HOLD	   220	// 掴んだ状態にする位置
+#define ARM_L_POSITION_RELEASE 450	// 離す位置（ゆっくりこの値に）
+#define ARM_U_POSITION_RELEASE 1100 // 離す位置（Lowerより先にこの値に）
+
 void mode_cone_loop(void);
+
+
+/**
+ * コーンをすくい上げるポジションにセット
+ */
+void set_arm_insert(void);
+
+/**
+ * コーンを掴むポジションにセット
+ *
+ * upperアームをおろして，コーンを固定するポジションにセット
+ */
+void set_arm_grab(void);
+
+/**
+ * コーンを持ち上げる
+ *
+ * 持ち歩く歩くポジションで固定
+ */
+void set_arm_holdUp(void);
+
+/**
+ * コーンを下ろす
+ */
+void set_arm_release(void);
+
 
 #endif /* MODE_CONE_H */
