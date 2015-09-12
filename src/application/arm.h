040157e9c9ff5a81954b8ce15128e234910c93d0
kasai <fishmeat37820+git@gmail.com>
2015-09-13 02:14:40 +0900

    [add] 既存関数に対するコメント追加

--- src/application/arm.h
+++ src/application/arm.h
@@ -1,8 +1,20 @@
 #ifndef ARM_H
 #define ARM_H
 
+/**
+ * Init servo Pins for arms
+ *
+ * To controll the arms,
+ * this function must be called first.
+ */
 void init_arm(void);
 
+/**
+ * Move arms while keeping machine safety
+ *
+ * PWM値をサーボ用レジスタに代入．
+ * マシンが壊れない可動範囲内で動くことが保証される．
+ */
 void move_arms(int, int);
 
 #endif /* ARM_H */
